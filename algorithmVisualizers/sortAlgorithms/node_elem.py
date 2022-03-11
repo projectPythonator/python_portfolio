323509bdeb2d12147a7a78192257e63593909f31
Agis Daniels <Danjkstra@localhost.localdomain>
2022-03-11 14:50:27 -0500

    added in the main node part

---
+++ algorithmVisualizers/sortAlgorithms/node_elem.py
@@ -0,0 +1,23 @@
+from functools import total_ordering
+
+from node_cell.py import NodeCell
+
+
+@total_ordering
+class NodeElem:
+    def __init__(self, value: int, position: int):
+        self.key = value
+        self.index = position
+        self.cell = NodeCell(self.key)
+
+    def __eq__(self, other):
+        return (self.key, self.index) == (other.key, other.index)
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __lt__(self, other):
+        return (self.key, self.index) < (other.key, other.index)
+
+    def observe(self):
+        self.cell.make_observing()
