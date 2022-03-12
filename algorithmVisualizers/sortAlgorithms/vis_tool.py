d8cd4d0081cf967ca1084fee79ca9dc304fa38d9
Agis Daniels <Danjkstra@localhost.localdomain>
2022-03-12 06:41:23 -0500

    added update nodes to update a list of nodes that might not be whole list also we set win_height max for cells here

--- algorithmVisualizers/sortAlgorithms/vis_tool.py
+++ algorithmVisualizers/sortAlgorithms/vis_tool.py
@@ -1,6 +1,7 @@
 import pygame
 from colours import Colour
 from node_array import NodeArray
+from node_cell import NodeCell
 
 
 class VisTool:
@@ -8,6 +9,7 @@ class VisTool:
         self.window = pygame.display.set_mode((win_width, win_height))
         pygame.display.set_caption(display_name)
         self.vis_arr = NodeArray([])
+        NodeCell.win_height = win_height
 
     def assign_data_set(self, unsorted_array: list):
         self.vis_arr.assign_data_set(unsorted_array)
@@ -15,10 +17,9 @@ class VisTool:
     def reshuffle(self):
         self.vis_arr.shuffle_array(1)
 
-    def update_window(self, a, b):
-        self.window.fill(Colour.BLACK)
-        self.vis_arr.draw_ith(self.window, a)
-        self.vis_arr.draw_ith(self.window, b)
+    def update_nodes(self, nodes: list):
+        for ind in nodes:
+            self.vis_arr.draw_node(self.window, ind)
         pygame.display.update()
 
     def draw(self):
